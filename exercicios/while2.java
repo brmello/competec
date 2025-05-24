a8aa488a157f2e3f9a67ac3677124b496d5214b7
Bruna <165676904+brmello@users.noreply.github.com>
2025-05-24 10:37:19 -0300

    Update while2.java

--- exercicios/while2.java
+++ exercicios/while2.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class while2 {
 	public static void main(String[] args) {
 	    int x = 0;
+	    int somaTotal =0;
 	    
 	    while (x < 101) {
-	        System.out.println(x + " ");
 	        x++;
+	        somaTotal += x;
 	    }
+	    
+	    System.out.println("A soma total = " + somaTotal);
 	}
 }
