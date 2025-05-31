2a6a67173d1a6093d8dbb2e504cc51236cc7631d
Bruna <165676904+brmello@users.noreply.github.com>
2025-05-31 10:24:44 -0300

    Add files via upload

---
+++ exercicios/ex7.java
@@ -0,0 +1,11 @@
+public class ex7
+{
+	public static void main(String[] args) {
+		int x = 0;
+
+		while (x <= 1000) {
+			System.out.println(x);
+			x+=10;
+		}
+	}
+}
---
+++ exercicios/ex8.java
@@ -0,0 +1,14 @@
+public class ex8
+{
+	public static void main(String[] args) {
+	    
+	    int numero[] = new int[3];
+	        numero[0] = 4;
+	        numero[1] = 6;
+	        numero[2] = 2;
+	        
+	    int soma = numero[0] + numero[1] + numero[2];
+	    
+	    System.out.print("Soma é = " + soma);
+	}
+}
